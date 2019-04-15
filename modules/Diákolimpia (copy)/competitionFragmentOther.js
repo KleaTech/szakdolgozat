66cb5640fce21b7157b6da58206719898af3996d
KleaTech <kleavenae@gmail.com>
2019-04-16 00:19:32 +0200

    minor updates external resources

---
+++ modules/Diákolimpia (copy)/competitionFragmentOther.js
@@ -0,0 +1,4 @@
+function apply(doubleList) {
+    var result = (doubleList[0] + doubleList[1] + doubleList[2])/3.0/2.0;
+    return result;
+}
--- modules/Diákolimpia/competitionFragmentJump_generated.js
+++ modules/Diákolimpia/competitionFragmentJump_generated.js
@@ -1,4 +1,5 @@
 function apply(doubleList) {
-    var array = [(doubleList[0] + doubleList[1] + doubleList[2])/3.0, (doubleList[3] + doubleList[4] + doubleList[5])/3.0];
+    var array = [Math.max(doubleList[0], doubleList[1], doubleList[2]),
+                 Math.max(doubleList[3], doubleList[4], doubleList[5])]
     return (array[0] + array[1])/2.0;
 }
