0abeb62f95d74ba138046e9131027762a84da9c9
KleaTech <kleavenae@gmail.com>
2019-05-01 18:19:07 +0200

    data.sql

--- hu.kleatech.jigsaw/src/main/java/hu/kleatech/jigsaw/MainApplication.java
+++ hu.kleatech.jigsaw/src/main/java/hu/kleatech/jigsaw/MainApplication.java
@@ -1,12 +1,17 @@
 package hu.kleatech.jigsaw;
 
-import hu.kleatech.jigsaw.model.*;
+import hu.kleatech.jigsaw.model.Round;
+import hu.kleatech.jigsaw.model.Sex;
 import hu.kleatech.jigsaw.scripting.*;
-import hu.kleatech.jigsaw.service.interfaces.*;
+import hu.kleatech.jigsaw.service.interfaces.CompetitionService;
+import hu.kleatech.jigsaw.service.interfaces.ParticipantService;
+import hu.kleatech.jigsaw.service.interfaces.RoundService;
 import static hu.kleatech.jigsaw.utils.Constants.*;
+import static hu.kleatech.jigsaw.utils.Utils.*;
 import java.io.File;
 import java.nio.file.Files;
-import java.time.LocalDate;
+import java.nio.file.Paths;
+import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,7 +31,7 @@ public class MainApplication implements ApplicationRunner {
     @Autowired SpringTemplateEngine templateEngine;
     @EventListener(ApplicationStartedEvent.class)
     public void load() {
-        EngineProvider.load();
+        SecureEngineProvider.load();
         setModuleTemplateResolver();
     }
     
@@ -55,33 +60,35 @@ public class MainApplication implements ApplicationRunner {
             SpringApplication.run(MainApplication.class, args);
     }
 
-    //TEST DATA
-    @Autowired CompetitionService competitionService;
-    @Autowired EventGroupService eventGroupService;
-    @Autowired EventService eventService;
-    @Autowired ParticipantService participantService;
-    @Autowired RoundService roundService;
-    @Autowired TeamService teamService;
-    @Autowired ManifestHandlerService manifestHandlerService;
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        //System.out.println(Dispatcher.getEngineProvider().getEngine(USER_DIR.resolve(MODULES_DIR_NAME).resolve("Diákolimpia")).preresults("competitionFragmentJump_generated_pre.js").apply(List.of(1d,2d,3d,4d,5d,6d)));
-        //System.out.println(Dispatcher.getEngineProvider().getEngine(USER_DIR.resolve(MODULES_DIR_NAME).resolve("Diákolimpia")).preresults("competitionFragmentOther_generated.js").apply(List.of(1d,2d,3d)));
     }
 
+    @Autowired ParticipantService participantService;
+    @Autowired CompetitionService competitionService;
+    @Autowired RoundService roundService;
+    
     @EventListener(ApplicationReadyEvent.class)
     @Order(10)
     void runAtReady() {
-        EventGroup eventGroup = eventGroupService.getAll().get(0);
-            Team team1 = teamService.add("Avasi", TEAM_FRAGMENT, eventGroup, null);
-                    participantService.add("Példa Lajos", LocalDate.now().minusYears(18), Sex.NOT_GIVEN, team1, null);
-            Team team2 = teamService.add("Diósgyőri", TEAM_FRAGMENT, eventGroup, null);
-                    participantService.add("Kovács István", LocalDate.now().minusYears(20), Sex.MALE, team2, null);
-                    participantService.add("Tóth Noémi", LocalDate.now().minusYears(21), Sex.FEMALE, team2, null);
-            Team team3 = teamService.add("Jezsuita", TEAM_FRAGMENT, eventGroup, null);
-                    participantService.add("Kovács Mátyás", LocalDate.now().minusYears(17), Sex.MALE, team3, null);
-                    participantService.add("Fazekas Ibolya", LocalDate.now().minusYears(18), Sex.FEMALE, team3, null);
-        //System.out.println(((SecureEngine)Dispatcher.getEngineProvider().getEngine(USER_DIR.resolve(MODULES_DIR_NAME).resolve("Diákolimpia"))).test().getAsDouble());
+        Try(() -> {
+            var source = USER_DIR.resolve("data.sql");
+            Files.move(source, source.resolveSibling("loaded_data.sql"));
+        });
+        //Test data can be added to the database here
+        var rand = new Random();
+        participantService.getAll().forEach(participant -> 
+            competitionService.getAll().forEach(competition -> {
+                if (competition.getEvent().getName().contains("ány") && participant.getSex() != Sex.FEMALE) return;
+                if (competition.getEvent().getName().contains("iú") && participant.getSex() != Sex.MALE) return;
+                var round = new Round(participant, competition, null);
+                var lower = rand.doubles(5, 10).findFirst().getAsDouble();
+                var upper = rand.doubles(lower, 15).findFirst().getAsDouble();
+                var stream = rand.doubles(lower, upper).map(d -> Math.round(d * 1000.0)/1000d);
+                if (competition.getName().contains("ávol")) stream.limit(6).forEach(round::add);
+                else stream.limit(3).forEach(round::add);
+                roundService.add(round);
+            })
+        );
     }
 }
---
+++ modules/Diákolimpia példa adatokkal/competitionFragmentOther.groovy
@@ -0,0 +1,4 @@
+def apply(doubleList) {
+    def result = (doubleList[0] + doubleList[1] + doubleList[2])/3.0;
+    return result;
+}
